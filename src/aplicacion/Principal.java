befbc98272e167bc12964e8647f52ab771662c8c
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:51:00 +0100

    ej12 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,12 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_12;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Dime un número ");
+        int num=sc.nextInt();
+        System.out.println(" El elemento "+num+" de la sucesión de fibonacci es  "+ej2_12.fib(num));
+
+    }
 }
--- src/dominio/ej2_12.java
+++ src/dominio/ej2_12.java
@@ -1,2 +1,16 @@
-package dominio;public class ej2_12 {
+package dominio;
+
+public class ej2_12 {
+
+
+    public static int fib(int n) {
+        if(n<=1) {
+            return n;
+        }else {
+            return fib(n-1)+fib(n-2);
+        }
+
+    }
+
 }
+
